7819dd4bc1f29e33040c3085b266b53a58a44132
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-03-09 02:47:23 +0200

    modified:   src/app/app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project-details/:slug',
+    renderMode: RenderMode.Server 
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
